6a1ba0b31d982fb3d04f171f6e53619304f48358
Chris Sepic <chris.sepic@gmail.com>
2010-05-09 12:03:32 -0500

    font and theme

---
+++ csepic/colors.el
@@ -0,0 +1,3 @@
+(require 'color-theme)
+(load-file "~/.emacs.d/vendor/color-theme/themes/color-theme-railscasts.el")
+(color-theme-railscasts)
---
+++ csepic/fonts.el
@@ -0,0 +1,2 @@
+(set-default-font "-apple-Bitstream_Vera_Sans_Mono-medium-normal-normal-*-*-*-*-*-m-0-iso10646-1")
+(set-face-attribute 'default nil :height 160)
