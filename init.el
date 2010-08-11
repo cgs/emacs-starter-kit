cb8cb71da6476acbbd55cb2fc053b7bbaf0d2b1b
Chris Sepic <chris.sepic@gmail.com>
2010-08-11 13:38:50 -0500

    bindings for ctags

--- init.el
+++ init.el
@@ -113,4 +113,14 @@
 (require 'confluence)
 (setq confluence-url "http://confluence/rpc/xmlrpc")
 
+(defvar my-keys-minor-mode-map (make-keymap) "my-keys-minor-mode keymap.")
+(define-key my-keys-minor-mode-map [(super t)] 'find-tag)
+(define-minor-mode my-keys-minor-mode
+  "A minor mode so that my key settings override annoying major modes."
+  t " my-keys" 'my-keys-minor-mode-map)
+(my-keys-minor-mode 1)
+(defun my-minibuffer-setup-hook ()
+  (my-keys-minor-mode 0))
+(add-hook 'minibuffer-setup-hook 'my-minibuffer-setup-hook)
+
 ;;; init.el ends here
